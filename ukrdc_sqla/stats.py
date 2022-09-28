f6a1bad555c196f311082297c212f5e743f41fab
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-09-28 13:31:22 +0100

    Remove now-unused facility stats tables

--- ukrdc_sqla/stats.py
+++ ukrdc_sqla/stats.py
@@ -1,41 +1,8 @@
-"""Models which relate to the generated statistics database"""
+"""Models which relate to the generated facility error stats and data health database"""
 from sqlalchemy import Column, Date, DateTime, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-
-class FacilityStats(Base):
-
-    __tablename__ = "facility_stats"
-
-    facility = Column(String, primary_key=True)
-
-    # Total distinct UKRDC IDs for the facility
-    total_patients = Column(Integer)
-
-    # Total distinct NIs in the errordb for the facility
-    patients_receiving_messages = Column(Integer)
-
-    # Distinct NIs in the errordb for the facility most recently receiving errors
-    patients_receiving_errors = Column(Integer)
-
-    last_updated = Column(DateTime)
-
-
-class FacilityLatestMessages(Base):
-
-    __tablename__ = "facility_latest_messages"
-
-    facility = Column(String, primary_key=True)
-
-    # ID of the most recent message received for the facility
-    last_message_received_id = Column(Integer)
-
-    # Date of the most recent message received for the facility
-    last_message_received_at = Column(DateTime)
-
-    last_updated = Column(DateTime)
 
 
 class ErrorHistory(Base):
@@ -45,21 +12,6 @@ class ErrorHistory(Base):
     facility = Column(String, primary_key=True)
     date = Column(Date, primary_key=True)
     count = Column(Integer)
-
-
-class PatientsLatestErrors(Base):
-
-    __tablename__ = "patients_latest_errors"
-
-    # Patient NI. Should be unique combined with facility as a patient only has one
-    # latest message per facility.
-    ni = Column(String, primary_key=True)
-    facility = Column(String, primary_key=True)
-
-    # The latest error message ID (primary key, not Mirth message ID) for the patient
-    id = Column(Integer)
-
-    last_updated = Column(DateTime)
 
 
 class MultipleUKRDCID(Base):
