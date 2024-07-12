1bb86725d8ee2dd43c19b5945d12248bb69b0f71
phil <philip.main@renalregistry.nhs.uk>
2024-07-12 15:17:48 +0100

    fixed typo

--- ukrdc_sqla/ukrdc.py
+++ ukrdc_sqla/ukrdc.py
@@ -1672,7 +1672,7 @@ class RRDataDefinition(Base):
     upload_key = Column(String(5), primary_key=True)
 
     table_name = Column("TABLE_NAME", String(30), nullable=False)
-    feild_name = Column(String(30), nullable=False)
+    field_name = Column(String(30), nullable=False)
     code_id = Column(String(10))
     mandatory = Column(Numeric(1, 0))
 
