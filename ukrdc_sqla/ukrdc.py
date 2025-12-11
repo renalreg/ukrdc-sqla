43aa106fadbd7ae10fee3773b743e3f63508c9be
OliveraReeves <oliver.reeves@ukkidney.org>
2025-12-11 09:49:55 +0000

    update type hints

--- ukrdc_sqla/ukrdc.py
+++ ukrdc_sqla/ukrdc.py
@@ -2,7 +2,7 @@
 
 import datetime
 from dataclasses import dataclass
-from typing import List, Optional, Union, Tuple, Any, Iterable
+from typing import List, Optional, Union, Tuple, Any
 
 from sqlalchemy import (
     Boolean,
@@ -28,7 +28,6 @@ from sqlalchemy.orm import (
     declarative_base,
     DynamicMapped,
     mapped_column,
-    InstrumentedAttribute,
 )
 
 
@@ -67,38 +66,6 @@ class Column(Col):
     @property
     def sqla_info(self) -> Optional[ColumnInfo]:
         return self.info.get("sqla_info")
-
-
-def column_name(col: Union[Col, InstrumentedAttribute]) -> str:
-    """
-    Return the column name for a single SQLAlchemy InstrumentedAttribute.
-
-    Example:
-        column_name(User.id) -> "id"
-    """
-    return col.name
-
-def column_names(
-    *items: Union[Union[Col, InstrumentedAttribute], Iterable[Union[Col, InstrumentedAttribute]]],
-) -> List[str]:
-    """
-    Return a list of column names for one or more SQLAlchemy InstrumentedAttributes.
-
-    Examples:
-        column_names(User.id)                  -> ["id"]
-        column_names(User.id, User.age)        -> ["id", "age"]
-        column_names([User.id, User.age])      -> ["id", "age"]
-    """
-    names: List[str] = []
-
-    for item in items:
-        if isinstance(item, Iterable):
-            for x in item:
-                names.append(x.name)
-        else:
-            names.append(item.name)
-
-    return names
 
 
 metadata = MetaData()
