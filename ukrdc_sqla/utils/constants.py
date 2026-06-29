f8e9ec4be6cea421b077f02b3846108d4381b70d
OliveraReeves <oliver.reeves@ukkidney.org>
2026-06-29 19:35:28 +0100

    ruff

--- ukrdc_sqla/utils/constants.py
+++ ukrdc_sqla/utils/constants.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class RelationshipType:
     """Links to the vwe_facility_relationship table, relationshiptype column"""
+
     feedshare: str = "FEED-SHARE"
     main_satellite: str = "MAIN-SATELLITE"
 
@@ -11,6 +12,7 @@ class RelationshipType:
 @dataclass
 class CodeMapFacilityType:
     """Links to the code_map table, source_coding_standard and destination_coding_standard columns"""
+
     satellite: str = "RR1+_SATELLITE"
     main: str = "RR1+_MAIN"
     feedshare_child: str = "RR1+_FEEDSHARE_CHILD"
