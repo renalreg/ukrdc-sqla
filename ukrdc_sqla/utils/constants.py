8dba61070d00786a0ea7d0f71bcbbc0aeefddb31
OliveraReeves <oliver.reeves@ukkidney.org>
2026-06-29 19:24:38 +0100

    feat(UK-197):
    as part of this and mainly for quarterly extract and other bits of code, I thought it would be better to have constants stored in the sqla library rather than having to look them up each time and redefine them through out the code bases, this could also be expanded with more constants going forward

---
+++ ukrdc_sqla/utils/constants.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RelationshipType:
+    """Links to the vwe_facility_relationship table, relationshiptype column"""
+    feedshare: str = "FEED-SHARE"
+    main_satellite: str = "MAIN-SATELLITE"
+
+
+@dataclass
+class CodeMapFacility:
+    """Links to the code_map table, source_coding_standard and destination_coding_standard columns"""
+    satellite: str = "RR1+_SATELLITE"
+    main: str = "RR1+_MAIN"
+    feedshare_child: str = "RR1+_FEEDSHARE_CHILD"
+    feedshare_parent: str = "RR1+_FEEDSHARE_PARENT"
