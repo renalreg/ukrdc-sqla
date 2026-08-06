edcc79195ad43c6424abf449446a29a03a9f3d37
OliveraReeves <158471173+OliveraReeves@users.noreply.github.com>
2026-08-06 14:08:33 +0100

    feat(UK-29): added flag to disable for specific sending facilities (#182)

--- ukrdc_sqla/ukrdc.py
+++ ukrdc_sqla/ukrdc.py
@@ -2496,6 +2496,9 @@ class Facility(Base):
     pvoutpkb: Mapped[bool] = mapped_column(
         "pvoutpkb", Boolean, nullable=False, server_default=text("false")
     )
+    enable_radar_export: Mapped[bool] = mapped_column(
+        Boolean, nullable=False, server_default=text("true")
+    )
     startdate: Mapped[datetime | None] = mapped_column("startdate", DateTime)
     enddate: Mapped[datetime | None] = mapped_column("enddate", DateTime)
     firstdataquarter: Mapped[int | None] = mapped_column("firstdataquarter", Integer)
