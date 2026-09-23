297d42088e86699efc656613fbda7a713fa84c10
OliveraReeves <158471173+OliveraReeves@users.noreply.github.com>
2026-09-23 15:19:27 +0100

    feat(TYPES): added extra types (#184)

--- ukrdc_sqla/utils/constants.py
+++ ukrdc_sqla/utils/constants.py
@@ -38,3 +38,24 @@ class GpType:
 
     gp: str = "GP"
     practice: str = "PRACTICE"
+
+
+@dataclass
+class StandardCodesType:
+    """Links to the various tables and codingstandards that can be used"""
+
+    pv: str = "PV"
+    ukrdc: str = "UKRDC"
+    ukrr: str = "UKRR"
+    snomed: str = "SNOMED"
+
+
+@dataclass
+class ConstantTypes(
+    RelationshipType,
+    CodeMapFacilityType,
+    FacilityType,
+    GpType,
+    StandardCodesType,
+):
+    """Combined set of all constant values defined in this file."""
